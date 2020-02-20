64d07ff31f2c1fcdd3e2787cd0961b6718f9fc14
Gaurav Pant <gaurav@squadx.io>
2020-02-20 16:46:31 +0530

    added new contract

--- contracts/hello.sol
+++ contracts/hello.sol
@@ -1,10 +1,29 @@
-pragma solidity  ^0.4.23;
+pragma solidity 0.4.23;
 
-contract StateVariablesAndFunctions {
-
-    //state variable
-    string statement;
-
-    //set statement function
-    function setStatement(string _statement) public  {
-        statement 
+contract FundRaising {
+    address owner = 0x17EF669115293087BC507dDc5e4d6b48209e32d5;
+    uint public total = 0;
+    
+    modifier ownerOnly() {
+        require(msg.sender == owner,"You must be owner");
+        _;
+    }
+    
+    //add funds
+    function add() payable public {
+        require(msg.value > 0, "You need to send some ether");
+        total += msg.value;
+    }
+    
+    //only onwer can withdraw funds
+    function withdrawOwner() public ownerOnly {
+        owner.transfer(address(this).balance);
+        total = 0;
+        assert(this.balance == total);
+    }
+    
+    //total funds collected
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+}
